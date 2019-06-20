018f7e7e05ab0ad278be046287b74f457e244283
Jared Lyvers <jared.lyvers@ldnddev.com>
2019-06-20 17:40:45 -0500

    fixed parallax for items further down the page

--- assets/js/ldnddev_hero.js
+++ assets/js/ldnddev_hero.js
@@ -1,18 +1,18 @@
-jQuery(document).ready(function($){    
-    if( $('.ldnddev-hero').hasClass('-parallax') ){
-        var speed = $('.ldnddev-hero.-parallax').attr('data-speed');
-        speed = '0.'+speed;
-        $('.ldnddev-hero.-parallax .ldnddev-hero__image').each(function(){
-            var $elem = $(this);
-            var $elem_content = $(this).parent().children('.ldnddev-hero__content');
+jQuery(document).ready(function($){
+    if( $('.ldnddev-hero').hasClass('-parallax') ){        
+        $('.ldnddev-hero.-parallax').each(function(){
+            var speed = $('.ldnddev-hero.-parallax').attr('data-speed');
+            speed = '0.'+speed;
+            var elem = $(this).find('.ldnddev-hero__content');
+            var offset = $(this).offset();
+            offset = (offset.top - 50);
             $(window).on('scroll', function(){
                 var scroll = $(document).scrollTop();
-                // $elem.css({
-                //     'background-position': '50% ' +(speed*scroll)+'px'
-                // });
-                $elem_content.css({
-                    'bottom': (speed*scroll)+'px'
-                });
+                if( $(this).scrollTop() >= offset ){
+                    elem.css({
+                        'bottom': (speed*(scroll-offset))+'px'
+                    });
+                }
             });
         });
     }
